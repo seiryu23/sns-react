84b2acd10d8caacf71d196d2b17e6f156f3577f0
seiryu23 <64452921+seiryu23@users.noreply.github.com>
2023-07-03 04:05:10 +0900

    done

---
+++ src/lib/apiClient.ts
@@ -0,0 +1,11 @@
+// import
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: "http://localhost:5000/api",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+export default apiClient;
